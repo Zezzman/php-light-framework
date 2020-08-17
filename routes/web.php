6739c655486dd4cdef04f04026a238cd48fcd589
Zezzman <francoisleroux97@gmail.com>
2020-08-18 01:31:02 +1000

    .

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,8 @@
  *  Documentation
  */
 $this->get('/', 'Home@Index');
-$this->get('home/', 'Home@Document');
-$this->get('home/index', 'Home@Document');
-$this->get('home/{index}', 'Home@Document');
-$this->get('{home}/{index}', 'Home@Document');
-$this->get('{home}/index', 'Home@Document');
-$this->get('home/index/{next}', 'Home@Document');
-$this->get('home/{next}...', 'Home@Document');
-$this->get('home/{index}/{next}...', 'Home@Document');
+$this->get('document/', 'Home@Document');
+/**
+ *  Image Convert to Data
+ */
+$this->request('imageData/', 'Storage@ImageConvert');
