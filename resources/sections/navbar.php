af2ea38213806aad4e7da69e3123053ec1976a5a
Zezzman <francoisleroux97@gmail.com>
2020-09-01 21:45:50 +1000

    navbar section clean up

--- resources/sections/navbar.php
+++ resources/sections/navbar.php
@@ -2,99 +2,49 @@
 /**
  * Generates Navbar from $bag
  */
-if (isset($bag['links'])) {
-    $uri = $this->controller->getRequest()->uri;
-    $links = $bag['links'];
-    $left = '';
-    $right = '';
-    foreach ($links as $name => $link) {
-        if (! isset($link['hide']) || $link['hide'] === false) {
-            if (isset($link['link'])) {
-                $linkObj = [
-                    'name' => $name,
-                    'link' => $link['link'],
-                    'active' => ($link['link'] === $uri) ? 'active' : '',
-                    'prepend' => $link['prepend'] ?? '',
-                    'append' => $link['append'] ?? '',
-                ];
-                if (isset($link['target']))
-                {
-                    $linkObj['target'] = 'target="'. $link['target'] . '"';
-                }
-                $item = System\Helpers\QueryHelper::scanCodes($linkObj, '<li class="nav-item {active}"><a class="nav-link" href="{link}" {target}>{prepend}{name}{append}</a></li>');
-                if (isset($link['align'])) {
-                    if ($link['align'] === 'right') {
-                        $right .= $item;
-                    } else {
-                        $left .= $item;
-                    }
-                } else {
-                    $left .= $item;
-                }
-            } elseif (isset($link['mail'])) {
-                $linkObj = [
-                    'name' => $name,
-                    'mail' => $link['mail'],
-                    'prepend' => $link['prepend'] ?? '',
-                    'append' => $link['append'] ?? '',
-                ];
-                $item = System\Helpers\QueryHelper::scanCodes($linkObj, '<li class="nav-item"><a class="nav-link nav-mail" href="mailto:{mail}">{prepend}{name}{append}</a></li>');
-                if (isset($link['align'])) {
-                    if ($link['align'] === 'right') {
-                        $right .= $item;
-                    } else {
-                        $left .= $item;
-                    }
-                } else {
-                    $left .= $item;
-                }
-            }  elseif (isset($link['button'])) {
-                $linkObj = [
-                    'name' => $name,
-                    'id' => $link['button'],
-                    'style' => $link['style'] ?? '',
-                    'class' => $link['class'] ?? '',
-                    'prepend' => $link['prepend'] ?? '',
-                    'append' => $link['append'] ?? '',
-                ];
-                if (isset($link['data']))
-                {
-                    $linkObj['data'] = '';
-                    foreach ($link['data'] as $name => $value) {
-                        $linkObj['data'] .= "data-$name=\"$value\" ";
-                    }
-                }
-                $item = System\Helpers\QueryHelper::scanCodes($linkObj, '<li class="nav-item"><button class="btn nav-link nav-button {class}" id="{id}" style="{style}" {data}>{prepend}{name}{append}</button></li>');
-                if (isset($link['align'])) {
-                    if ($link['align'] === 'right') {
-                        $right .= $item;
-                    } else {
-                        $left .= $item;
-                    }
-                } else {
-                    $left .= $item;
-                }
-            }
+if (empty($bag['links'])) return;
+
+$uri = $this->controller->getRequest()->uri;
+$links = $bag['links'];
+$left = '';
+$right = '';
+foreach ($links as $name => $link) {
+    if (($link['hide'] ?? true) === false) continue;
+    $navItem = $link;
+    $navItem['name'] = $link['name'] ?? $name;
+    if (isset($link['target'])) $navItem['target'] = 'target="'. $link['target'] . '"';
+    if (isset($link['data']))
+    {
+        $data = '';
+        foreach ($link['data'] as $name => $value) {
+            $data .= "data-$name=\"$value\" ";
         }
+        $navItem['data'] = $data;
     }
-    if (! empty($left))
-    {
-        $left = '<ul class="navbar-nav mr-auto">'. $left . '</ul>';
+    if (isset($link['link'])) {
+        $style = '<li class="nav-item '. (($link['link'] === $uri) ? 'active' : '').
+            '"><a class="nav-link {class}" href="{link}" {target} {data}>{prepend}{name}{append}</a></li>';
+    } elseif (isset($link['button'])) {
+        $style = '<li class="nav-item"><button class="btn nav-link nav-button {class}" '.
+            'id="{button}" style="{style}" {data}>{prepend}{name}{append}</button></li>';
     }
-    if (! empty($right))
-    {
-        $right = '<ul class="navbar-nav ml-auto">' . $right . '</ul>';
+    $item = System\Helpers\QueryHelper::scanCodes($navItem, $style ?? '');
+    if (isset($link['align'])) {
+        if ($link['align'] === 'right') {
+            $right .= $item;
+        } else {
+            $left .= $item;
+        }
+    } else {
+        $left .= $item;
     }
-    $brand = '';
-    $entries = '';
-    if (! empty($navBrand = ($bag['BRAND'] ?? false)))
-    {
-        $brand = '<a class="navbar-brand" href="' . $navBrand['link'] . '">' . $navBrand['name'] . '</a>';
-    }
-    if (! empty($left) || ! empty($right))
-    {
-        $entries = '<div class="collapse navbar-collapse" id="navbar-collapse-div">' . $left . $right . '</div>';
-    }
-    $this->card(config('LAYOUT.NAV'), ['brand' => $brand, 'entries' => $entries]);
 }
-?>
+if (! empty($left)) $left = '<ul class="navbar-nav mr-auto">'. $left . '</ul>';
+if (! empty($right)) $right = '<ul class="navbar-nav ml-auto">' . $right . '</ul>';
+
+if (! empty($navBrand = ($bag['BRAND'] ?? false)))
+    $brand = '<a class="navbar-brand" href="' . $navBrand['link'] . '">' . $navBrand['name'] . '</a>';
+if (! empty($left) || ! empty($right))
+    $entries = '<div class="collapse navbar-collapse" id="navbar-collapse-div">' . $left . $right . '</div>';
+
+$this->card(config('LAYOUT.NAV'), ['brand' => ($brand ?? ''), 'entries' => ($entries ?? '')]);
